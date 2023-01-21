9e0965bb1ed18832c930c47e9c73203e9ca8bea5
Eli Baynes <sbaynes1994@gmail.com>
2023-01-20 22:38:56 -0600

    add database models

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,7 @@
+// import dependencies
 const express = require('express');
 const routes = require('./routes');
-// import sequelize connection
+const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
